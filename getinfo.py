1d10582ce14dead7eaa77671181f67a00c37f78c
LKai-Xudexiaohao <derek@126.com>
2019-12-25 22:33:20 +0800

    update

---
+++ getinfo.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2
+import os
+import time
+
+s = set("abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ,./;'][}{\`~|:<>?-=+_")
+s.add('"')
+folder = os.getcwd()
+#print(folder)
+result = ""
+if not os.path.exists(folder+'\output'+'.txt'):
+    newfile = open(folder+'\output'+'.txt','w')
+    print("output file created")
+    newfile.close()
+
+out = open(folder+'\output'+'.txt','a')
+f = open('test.txt','r')
+for line in open('test.txt'):
+    tmp = f.readline()
+    tmp = tmp.strip('\n')
+    if (tmp in s):
+        result = result + tmp
+    elif tmp == "ENTER":
+        out.write(result)
+        out.write('\n')
+        result = ""
+    elif tmp == "SPACE":
+        result = result + " "
+    elif tmp == "TAB"
+        out.write(result)
+        out.write('\n')
+        result = ""
+
+out.write(result)
+out.write('\n')
+t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+out.write(str(t))
+out.write('\n')
+f.close()
+out.close()
+
+print("process finished")
---
+++ getpid.c
@@ -0,0 +1,82 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define READ_BUF_SIZE 1024
+long* find_pid_by_name( char* pidName)
+{
+   DIR *dir;
+   struct dirent *next;
+   long* pidList=NULL;
+   int i=0;
+
+   ///proc中包括当前的进程信息,读取该目录
+   dir = opendir("/proc");
+   if (!dir)
+   {
+   //    perror_msg_and_die("Cannot open /proc");
+       printf("cannot open /proc");
+   }
+
+   //遍历
+   while ((next = readdir(dir)) != NULL)
+   {
+       FILE *status;
+       char filename[READ_BUF_SIZE];
+       char buffer[READ_BUF_SIZE];
+       char name[READ_BUF_SIZE];
+
+       /* Must skip ".." since that is outside /proc */
+       if (strcmp(next->d_name, "..") == 0)
+           continue;
+
+       /* If it isn't a number, we don't want it */
+       if (!isdigit(*next->d_name))
+           continue;
+       //设置进程
+      sprintf(filename, "/proc/%s/status", next->d_name);
+    // printf("filename is :%s\n",filename);
+       if (! (status = fopen(filename, "r")) )
+       {
+           continue;
+       }
+       if (fgets(buffer, READ_BUF_SIZE-1, status) == NULL)
+       {
+           fclose(status);
+           continue;
+       }
+       fclose(status);
+
+       //得到进程id
+       /* Buffer should contain a string like "Name:   binary_name" */
+       sscanf(buffer, "%*s %s", name);
+       if (strcmp(name, pidName) == 0)
+       {
+           printf("name:%s\n",name);
+           pidList=realloc( pidList, sizeof(long) * (i+2));
+           pidList[i++]=strtol(next->d_name, NULL, 0);
+           break;
+       }
+   }
+
+   if (pidList)
+   {
+       pidList[i]=0;
+       printf("pid:%ld\n",pidList[0]);
+
+       return pidList[0];
+   }
+   return NULL;
+}
+
+void main()
+{
+   int pid;
+   char cmd[20]={0};
+   pid = find_pid_by_name( "firefox");
+   printf("%s : %d \n","firefox",pid);
+}
